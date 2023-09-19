fc53065707ade46614ae11b6953e4582af075ba3
Cansero <ivanmena0011@gmail.com>
2023-09-19 11:26:04 -0400

    Functions and debugging

--- ch2/functions.c
+++ ch2/functions.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int power(int base, int n) {
-    
+    int r = base;
+    for (int i = 0; i < n; i++) {
+        r = r * base;
+    }
+    return r;
 }
 
 int main() {
-
+    printf("%d\n", power(5, 2));
+    printf("%d\n", power(10, 3));
+    
     return 0;
 }
